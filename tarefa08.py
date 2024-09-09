d094936f2efc50cf8a6705c8f05409afdcb09197
Henrique88-wp <phdsn411@gmail.com>
2024-09-09 09:34:13 -0300

    atividade08

---
+++ tarefa08.py
@@ -0,0 +1,5 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+a = float(input("valor em reais que você carrega"))
+valor_do_dollar = 5.61
+final = a/valor_do_dollar
+print(f"você esta carregando {final} em dolár.")
